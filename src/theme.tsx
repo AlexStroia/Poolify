be00a8d6841786ff5a79a0c60838691326abf04e
alexandru.stroia <alexandru.stroia>
2023-07-02 11:38:39 -0400

    Setup App Theme

---
+++ src/theme.tsx
@@ -0,0 +1,42 @@
+import { ThemeOptions, createTheme } from '@mui/material';
+import { deepPurple, grey, orange, purple, teal } from '@mui/material/colors';
+
+const themeOptions : ThemeOptions= {
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: deepPurple[500],
+    },
+    error: {
+      main: teal[500],
+    },
+    success: {
+      main: orange[500],
+    },
+    background: {
+      default: '#ffffff',
+      paper: grey[50],
+    },
+
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+    fontSize: 14,
+    h1: {
+      fontSize: 26,
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: 24,
+      fontWeight: 700,
+    },
+    body1: {
+      fontSize: 16,
+      color: grey[800],
+    },
+  },
+}
+
+export const theme = createTheme(themeOptions);
